3b85cbe7af65c88b00062b7ebc62e28fa93faec9
Adihsurya <indivi241@gmail.com>
2018-05-24 21:44:26 +0700

    Cadangan Grafis
    Hanya Kubus

---
+++ GrafisAkhir.java
@@ -0,0 +1,152 @@
+package org.yourorghere;
+
+import com.sun.opengl.util.Animator;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+/**
+ * GrafisAkhir.java <BR>
+ * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
+ * <P>
+ *
+ * This version is equal to Brian Paul's version 1.2 1999/10/21
+ */
+public class GrafisAkhir implements GLEventListener {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame("Project Akhir Grafis");
+        GLCanvas canvas = new GLCanvas();
+
+        canvas.addGLEventListener(new GrafisAkhir());
+        frame.add(canvas);
+        frame.setSize(1050, 1200);
+        final Animator animator = new Animator(canvas);
+        frame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // Run this on another thread than the AWT event queue to
+                // make sure the call to Animator.stop() completes before
+                // exiting
+                new Thread(new Runnable() {
+
+                    public void run() {
+                        animator.stop();
+                        System.exit(0);
+                    }
+                }).start();
+            }
+        });
+        // Center frame
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        animator.start();
+    }
+
+    public void init(GLAutoDrawable drawable) {
+        // Use debug pipeline
+        // drawable.setGL(new DebugGL(drawable.getGL()));
+
+        GL gl = drawable.getGL();
+        System.err.println("INIT GL IS: " + gl.getClass().getName());
+
+        // Enable VSync
+        gl.setSwapInterval(1);
+
+        // Setup the drawing area and shading mode
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+
+        if (height <= 0) { // avoid a divide by zero error!
+
+            height = 1;
+        }
+        final float h = (float) width / (float) height;
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(45.0f, h, 1.0, 20.0);
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
+    public void display(GLAutoDrawable drawable) {
+        GL gl = drawable.getGL();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.5f, -0.5f, -6.0f);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.5f, 0.5f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glEnd();
+        
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.5f, -0.5f, -6.0f);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.5f, 0.5f, -1.5f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glVertex3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 1.0f);
+        gl.glEnd();
+        
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.5f, -0.5f, -6.0f);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.5f, 1.5f, -1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glEnd();
+        
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.5f, -0.5f, -6.0f);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(-1.5f, 0.5f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.0f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glEnd();
+        
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.5f, -0.5f, -6.0f);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(-1.5f, -0.2f, 1.3f);
+        gl.glVertex3f(0.0f, 1.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, 0.0f);
+        gl.glEnd();
+        
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.5f, -0.5f, -6.0f);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(-2.7f, 0.7f, -1.6f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glEnd();
+        
+        gl.glFlush();
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
+}
