9cf9b964df2bedbbf589a368357d4adc5bb3a851
Adihsurya <indivi241@gmail.com>
2018-07-02 13:52:09 +0700

    update()
    Kemarin salah upload project

--- GrafisAkhir/src/org/yourorghere/GrafisAkhir.java
+++ GrafisAkhir/src/org/yourorghere/GrafisAkhir.java
@@ -1,7 +1,11 @@
 package org.yourorghere;
 
 import com.sun.opengl.util.Animator;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.media.opengl.GL;
@@ -10,32 +14,19 @@ import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
-/**
- * GrafisAkhir.java <BR>
- * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
- * <P>
- *
- * This version is equal to Brian Paul's version 1.2 1999/10/21
- */
-public class GrafisAkhir implements GLEventListener {
+public class GrafisAkhir implements GLEventListener, MouseListener, MouseMotionListener {
 
     public static void main(String[] args) {
-        Frame frame = new Frame("Project Akhir Grafis");
+        Frame frame = new Frame("project Tugas Akhir Pemrogaman Grafis");
         GLCanvas canvas = new GLCanvas();
-
         canvas.addGLEventListener(new GrafisAkhir());
         frame.add(canvas);
-        frame.setSize(1050, 1200);
+        frame.setSize(1200, 800);
         final Animator animator = new Animator(canvas);
         frame.addWindowListener(new WindowAdapter() {
-
             @Override
             public void windowClosing(WindowEvent e) {
-                // Run this on another thread than the AWT event queue to
-                // make sure the call to Animator.stop() completes before
-                // exiting
                 new Thread(new Runnable() {
-
                     public void run() {
                         animator.stop();
                         System.exit(0);
@@ -43,110 +34,94 @@ public class GrafisAkhir implements GLEventListener {
                 }).start();
             }
         });
-        // Center frame
+// Center frame
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         animator.start();
     }
+    private float view_rotx = 20.0f;
+    private float view_roty = 30.0f;
+    private int oldMouseX;
+    private int oldMouseY;
 
     public void init(GLAutoDrawable drawable) {
-        // Use debug pipeline
-        // drawable.setGL(new DebugGL(drawable.getGL()));
-
         GL gl = drawable.getGL();
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
-
-        // Enable VSync
-        gl.setSwapInterval(1);
-
-        // Setup the drawing area and shading mode
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+        gl.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
+        gl.glShadeModel(GL.GL_FLAT);
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        gl.glEnable(GL.GL_NORMALIZE);
+        drawable.addMouseListener(this);
+        drawable.addMouseMotionListener(this);
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL gl = drawable.getGL();
         GLU glu = new GLU();
-
-        if (height <= 0) { // avoid a divide by zero error!
-
+        if (height <= 0) {
             height = 1;
         }
         final float h = (float) width / (float) height;
         gl.glViewport(0, 0, width, height);
         gl.glMatrixMode(GL.GL_PROJECTION);
         gl.glLoadIdentity();
-        glu.gluPerspective(45.0f, h, 1.0, 20.0);
+        glu.gluPerspective(45.0f, h, 1.0, 200.0);
         gl.glMatrixMode(GL.GL_MODELVIEW);
         gl.glLoadIdentity();
     }
 
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
+        GLU glu = new GLU();
+// Clear the drawing area
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+// Reset the current matrix to the "identity"
         gl.glLoadIdentity();
-        gl.glTranslatef(0.5f, -0.5f, -6.0f);
-        gl.glBegin(GL.GL_QUADS);
-        gl.glColor3f(1.5f, 0.5f, 1.0f);
-        gl.glVertex3f(0.0f, 0.0f, 0.0f);
-        gl.glVertex3f(0.0f, 0.0f, 1.0f);
-        gl.glVertex3f(1.0f, 0.0f, 1.0f);
-        gl.glVertex3f(1.0f, 0.0f, 0.0f);
-        gl.glEnd();
-        
-        gl.glLoadIdentity();
-        gl.glTranslatef(0.5f, -0.5f, -6.0f);
-        gl.glBegin(GL.GL_QUADS);
-        gl.glColor3f(1.5f, 0.5f, -1.5f);
-        gl.glVertex3f(1.0f, 0.0f, 0.0f);
-        gl.glVertex3f(1.0f, 1.0f, 0.0f);
-        gl.glVertex3f(1.0f, 1.0f, 1.0f);
-        gl.glVertex3f(1.0f, 0.0f, 1.0f);
-        gl.glEnd();
-        
-        gl.glLoadIdentity();
-        gl.glTranslatef(0.5f, -0.5f, -6.0f);
-        gl.glBegin(GL.GL_QUADS);
-        gl.glColor3f(1.5f, 1.5f, -1.0f);
-        gl.glVertex3f(0.0f, 0.0f, 0.0f);
-        gl.glVertex3f(0.0f, 1.0f, 0.0f);
-        gl.glVertex3f(1.0f, 1.0f, 0.0f);
-        gl.glVertex3f(1.0f, 0.0f, 0.0f);
-        gl.glEnd();
-        
-        gl.glLoadIdentity();
-        gl.glTranslatef(0.5f, -0.5f, -6.0f);
-        gl.glBegin(GL.GL_QUADS);
-        gl.glColor3f(-1.5f, 0.5f, 1.0f);
-        gl.glVertex3f(0.0f, 0.0f, 0.0f);
-        gl.glVertex3f(0.0f, 1.0f, 0.0f);
-        gl.glVertex3f(0.0f, 1.0f, 1.0f);
-        gl.glVertex3f(0.0f, 0.0f, 1.0f);
-        gl.glEnd();
-        
-        gl.glLoadIdentity();
-        gl.glTranslatef(0.5f, -0.5f, -6.0f);
-        gl.glBegin(GL.GL_QUADS);
-        gl.glColor3f(-1.5f, -0.2f, 1.3f);
-        gl.glVertex3f(0.0f, 1.0f, 0.0f);
-        gl.glVertex3f(0.0f, 1.0f, 1.0f);
-        gl.glVertex3f(1.0f, 1.0f, 1.0f);
-        gl.glVertex3f(1.0f, 1.0f, 0.0f);
-        gl.glEnd();
-        
-        gl.glLoadIdentity();
-        gl.glTranslatef(0.5f, -0.5f, -6.0f);
-        gl.glBegin(GL.GL_QUADS);
-        gl.glColor3f(-2.7f, 0.7f, -1.6f);
-        gl.glVertex3f(0.0f, 0.0f, 1.0f);
-        gl.glVertex3f(1.0f, 0.0f, 1.0f);
-        gl.glVertex3f(1.0f, 0.0f, 0.0f);
-        gl.glVertex3f(0.0f, 0.0f, 0.0f);
-        gl.glEnd();
-        
+        glu.gluLookAt(4, 4, 4, // eye pos
+                0, 0, 0, // look at
+                0, 0, 1); // up
+       gl.glTranslatef(0.5f, 0.5f, 0.5f);
+        gl.glRotatef(view_rotx, 1f, 0.0f, 0.0f);
+        gl.glRotatef(view_roty, 0f, 0.1f, 0.0f);
+        gl.glTranslatef(-1f, -10f, -10f);
+        Objek.baling(gl);
+//        Objek.kotak(gl);
+//        Objek.segitiga(gl);
+//        Objek.ekor(gl);
         gl.glFlush();
     }
 
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+        oldMouseX = e.getX();
+        oldMouseY = e.getY();
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        Dimension size = e.getComponent().getSize();
+        float thetaY = 360.0f * ((float) (x - oldMouseX) / (float) size.width);
+        float thetaX = 360.0f * ((float) (oldMouseY - y) / (float) size.height);
+        oldMouseX = x;
+        oldMouseY = y;
+        view_rotx += thetaX;
+        view_roty += thetaY;
+    }
+
+    public void mouseMoved(MouseEvent e) {
     }
 }
