faab7ec3fb20f89943b61c2eadfa37aca63f8db1
nikoedyr <39364571+nikoedyr@users.noreply.github.com>
2018-07-03 18:21:19 +0700

    ini heli yang sudah di gabung dengan baling2
    belum sempurna

---
+++ coba1/src/org/yourorghere/Coba1.java
@@ -0,0 +1,156 @@
+package org.yourorghere;
+import com.sun.opengl.util.Animator;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+public class Coba1 implements GLEventListener,MouseListener,MouseMotionListener {
+public static void main(String[] args) {
+Frame frame = new Frame("Baling-Baling");
+GLCanvas canvas = new GLCanvas();
+canvas.addGLEventListener(new Coba1());
+frame.add(canvas);
+frame.setSize(640, 480);
+final Animator animator = new Animator(canvas);
+frame.addWindowListener(new WindowAdapter() {
+@Override
+public void windowClosing(WindowEvent e) {
+new Thread(new Runnable() {
+public void run() {
+animator.stop();
+System.exit(0);
+}
+}).start();
+}
+});
+// Center frame
+frame.setLocationRelativeTo(null);
+frame.setVisible(true);
+animator.start();
+}
+private float view_rotx = 20.0f;
+private float view_roty = 30.0f;
+private int oldMouseX;
+private int oldMouseY;
+public void init(GLAutoDrawable drawable) {
+GL gl = drawable.getGL();
+gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+gl.glShadeModel(GL.GL_FLAT);
+float ambient[]={1.0f,1.0f,1.0f,1.0f};
+float difusse[]={1.0f,1.0f,1.0f,1.0f};
+float specular[]={0.2f,1.0f,0.2f,1.0f};
+float position[]={20.0f,30.0f,20.0f,0.0f};
+gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambient,0);
+gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, difusse,0);
+gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, position,0);
+gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, specular,0);
+float[] mambient ={ 0.1745f, 0.01175f, 0.01175f, 0.55f
+};
+float[] mdiffuse ={0.61424f, 0.04136f, 0.04136f, 0.55f };
+float[] mspecular ={0.727811f, 0.626959f, 0.626959f, 0.55f };
+float mshine =76.8f ;
+gl.glMaterialfv(GL.GL_FRONT,GL.GL_AMBIENT,mambient,0);
+gl.glMaterialfv(GL.GL_FRONT,GL.GL_DIFFUSE,mdiffuse,0);
+gl.glMaterialfv(GL.GL_FRONT,GL.GL_SPECULAR,mspecular, 0);
+gl.glMaterialf (GL.GL_FRONT,GL.GL_SHININESS,mshine);
+gl.glEnable(GL.GL_LIGHTING);
+gl.glEnable(GL.GL_LIGHT0);
+gl.glEnable(GL.GL_DEPTH_TEST);
+gl.glEnable(GL.GL_NORMALIZE);
+drawable.addMouseListener(this);
+drawable.addMouseMotionListener(this);
+}
+public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+GL gl = drawable.getGL();
+GLU glu = new GLU();
+if (height <= 0) {
+height = 1;
+}
+final float h = (float) width / (float) height;
+gl.glViewport(0, 0, width, height);
+gl.glMatrixMode(GL.GL_PROJECTION);
+gl.glLoadIdentity();
+glu.gluPerspective(45.0f, h, 1.0, 20.0);
+gl.glMatrixMode(GL.GL_MODELVIEW);
+gl.glLoadIdentity();
+}
+float angle = 1;
+float direction = 1;
+public void display(GLAutoDrawable drawable) {
+GL gl = drawable.getGL();
+GLU glu = new GLU();
+// Clear the drawing area
+gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+// Reset the current matrix to the "identity"
+gl.glLoadIdentity();
+glu.gluLookAt(10,10,10, // eye pos
+0,0,0, // look at
+0,0,1); // up
+gl.glRotatef(view_rotx, 1.0f, 0.0f, 0.0f);
+gl.glRotatef(view_roty, 0.0f, 1.0f, 0.0f);
+Objek.kotak(gl);
+gl.glPushMatrix();
+gl.glPushMatrix();
+gl.glPushMatrix();
+gl.glPushMatrix();
+gl.glTranslatef(0f, 0f, 0f);
+Objek.segitiga(gl);
+gl.glPopMatrix();
+gl.glTranslatef(9f, 1f, 0f);
+gl.glRotatef(angle, 0f, 5f, 0f);
+Objek.baling(gl);
+gl.glPopMatrix();
+gl.glTranslatef(1f, 1f, 0f);
+gl.glRotatef(angle, 0f, 0.0f, -5f);
+Objek.ekor3(gl);
+gl.glPopMatrix();
+gl.glTranslatef(1f, 1f, 0f);
+Objek.ekor(gl);
+gl.glPopMatrix();
+gl.glTranslatef(0f, 0f, 0f);
+Objek.ekor2(gl);
+gl.glPopMatrix();
+
+angle +=direction;
+if(angle>20){
+    direction=-direction;
+}else if(angle<-50){
+    direction=-direction;
+}
+angle += 0.2f;
+}
+public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+}
+public void mouseClicked(MouseEvent e){
+}
+public void mouseEntered(MouseEvent e){}
+public void mouseExited(MouseEvent e){
+}
+public void mouseReleased(MouseEvent e){
+}
+public void mousePressed(MouseEvent e){
+oldMouseX=e.getX();
+oldMouseY=e.getY();
+}
+public void mouseDragged(MouseEvent e){
+int x=e.getX();
+int y=e.getY();
+Dimension size=e.getComponent().getSize();
+float thetaY=360.0f*((float)(x-oldMouseX)/(float)size.width);
+float thetaX=360.0f*((float)(oldMouseY-y)/(float)size.height);
+oldMouseX=x;
+oldMouseY=y;
+view_rotx+=thetaX;
+view_roty+=thetaY;
+}
+public void mouseMoved(MouseEvent e){
+}
+}
---
+++ coba1/src/org/yourorghere/Objek.java
@@ -0,0 +1,319 @@
+package org.yourorghere;
+
+import com.sun.opengl.util.GLUT;
+import javax.media.opengl.GL;
+import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
+
+public class Objek {
+
+        static void kotak(GL gl) {
+             gl.glBegin(GL.GL_POLYGON);/*  bagian depan */
+        gl.glNormal3f(-4.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 3.0f);
+        gl.glVertex3f(4.0f, 0.0f, 3.0f);
+        gl.glVertex3f(4.0f, 0.0f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian bawah */
+        gl.glNormal3f(0.0f, 0.0f, -3.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(4.0f, 0.0f, 0.0f);
+        gl.glVertex3f(4.0f, 3.5f, 0.0f);
+        gl.glVertex3f(0.0f, 3.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian belakang */
+        gl.glNormal3f(4.0f, 0.0f, 0.0f);
+        gl.glVertex3f(4.0f, 3.5f, 0.0f);
+        gl.glVertex3f(4.0f, 3.5f, 3.0f);
+        gl.glVertex3f(0.0f, 3.5f, 3.0f);
+        gl.glVertex3f(0.0f, 3.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian atas */
+        gl.glNormal3f(0.0f, 0.0f, 3.0f);
+        gl.glVertex3f(4.0f, 3.5f, 3.0f);
+        gl.glVertex3f(4.0f, 0.0f, 3.0f);
+        gl.glVertex3f(0.0f, 0.0f, 3.0f);
+        gl.glVertex3f(0.0f, 4.0f, 3.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kiri */
+        gl.glNormal3f(0.0f, 3.5f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 3.5f, 0.0f);
+        gl.glVertex3f(0.0f, 3.5f, 3.0f);
+        gl.glVertex3f(0.0f, 0.0f, 3.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kanan */
+        gl.glNormal3f(0.0f, -3.5f, 0.0f);
+        gl.glVertex3f(4.0f, 0.0f, 0.0f);
+        gl.glVertex3f(4.0f, 0.0f, 3.0f);
+        gl.glVertex3f(4.0f, 3.5f, 3.0f);
+        gl.glVertex3f(4.0f, 3.5f, 0.0f);
+        gl.glEnd();
+        }
+        static void segitiga(GL gl) {
+           gl.glBegin(GL.GL_POLYGON);/*  bagian depan */
+        gl.glNormal3f(-4.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 3.0f);
+        gl.glVertex3f(-4.0f, 0.0f, 3.0f);
+        //gl.glVertex3f(-4.0f, 0.0f, 0.0f);
+        gl.glEnd();
+      
+        gl.glBegin(GL.GL_POLYGON);/*  bagian belakang */
+        gl.glNormal3f(4.0f, 0.0f, 0.0f);
+       // gl.glVertex3f(4.0f, 3.5f, 0.0f);
+        gl.glVertex3f(-4.0f, 3.5f, 3.0f);
+        gl.glVertex3f(0.0f, 3.5f, 3.0f);
+       gl.glVertex3f(0.0f, 3.5f, 0.0f);
+        gl.glEnd();
+       
+        gl.glBegin(GL.GL_POLYGON);/*  bagian atas */
+        gl.glNormal3f(0.0f, 0.0f, 3.0f);
+        gl.glVertex3f(-4.0f, 3.5f, 3.0f);
+        gl.glVertex3f(-4.0f, 0.0f, 3.0f);
+        gl.glVertex3f(0.0f, 0.0f, 3.0f);
+        gl.glVertex3f(0.0f, 4.0f, 3.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kiri */
+        gl.glNormal3f(0.0f, 3.5f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 3.5f, 0.0f);
+        gl.glVertex3f(0.0f, 3.5f, 3.0f);
+        gl.glVertex3f(0.0f, 0.0f, 3.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kanan */
+        gl.glNormal3f(0.0f, 3.5f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(-4.0f, 0.0f, 3.0f);
+        gl.glVertex3f(-4.0f, 3.5f, 3.0f);
+        gl.glVertex3f(0f, 3.5f, 0.0f);
+        gl.glEnd();
+    }
+        static void ekor(GL gl) {
+             gl.glBegin(GL.GL_POLYGON);/*  bagian depan */
+        gl.glNormal3f(-8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 0.0f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian bawah */
+        gl.glNormal3f(0.0f, 0.0f, -1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian belakang */
+        gl.glNormal3f(8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 1.5f, 0.0f);
+        gl.glVertex3f(8.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian atas */
+        gl.glNormal3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 1.5f, 1.0f);
+        gl.glVertex3f(8.0f, 0.0f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(0.0f, 1.0f, 1.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kiri */
+        gl.glNormal3f(0.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kanan */
+        gl.glNormal3f(0.0f, -1.5f, 0.0f);
+        gl.glVertex3f(8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 1.5f, 1.0f);
+        gl.glVertex3f(8.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        }
+        static void ekor2(GL gl) {
+             gl.glBegin(GL.GL_POLYGON);/*  bagian depan */
+        gl.glNormal3f(-1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian bawah */
+        gl.glNormal3f(0.0f, 0.0f, -1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian belakang */
+        gl.glNormal3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f, 1.5f, 0.0f);
+        gl.glVertex3f(1.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian atas */
+        gl.glNormal3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.5f, 1.0f);
+        gl.glVertex3f(1.0f, 0.0f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(0.0f, 1.0f, 1.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kiri */
+        gl.glNormal3f(0.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kanan */
+        gl.glNormal3f(0.0f, -1.5f, 0.0f);
+        gl.glVertex3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f, 0.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.5f, 1.0f);
+        gl.glVertex3f(1.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        }
+        static void baling(GL gl){
+       // gl.glTranslatef(0.5f, -0.2f, -6.0f);
+        
+        gl.glBegin(GL.GL_POLYGON);//depan
+        gl.glColor3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(2.0f, 0.02f, 0.05f);
+        gl.glVertex3f(2.0f, 0.0f, 0.05f);
+        gl.glVertex3f(-2.0f, 0.0f, 0.05f);
+        gl.glVertex3f(-2.0f, 0.02f, 0.05f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);//belakang
+        gl.glColor3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(2.0f, 0.02f, -0.05f);
+        gl.glVertex3f(2.0f, 0.0f, -0.05f);
+        gl.glVertex3f(-2.0f, 0.0f, -0.05f);
+        gl.glVertex3f(-2.0f, 0.02f, -0.05f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);//atas
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(2.0f, 0.02f, 0.05f);
+        gl.glVertex3f(2.0f, 0.02f, -0.05f);
+        gl.glVertex3f(-2.0f, 0.02f, -0.05f);
+        gl.glVertex3f(-2.0f, 0.02f, 0.05f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);//bawah
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(2.0f, 0.0f, 0.05f);
+        gl.glVertex3f(2.0f, 0.0f, -0.05f);
+        gl.glVertex3f(-2.0f, 0.0f, -0.05f);
+        gl.glVertex3f(-2.0f, 0.0f, 0.05f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);//atas 2
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, -2.0f);
+        gl.glVertex3f(0.0f, 0.0f, 2.0f);
+        gl.glVertex3f(-0.1f, 0.0f, 2.0f);
+        gl.glVertex3f(-0.1f, 0.0f, -2.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);//bawah 2
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(0.0f, -0.02f, -2.0f);
+        gl.glVertex3f(0.0f, -0.02f, 2.0f);
+        gl.glVertex3f(-0.1f, -0.02f, 2.0f);
+        gl.glVertex3f(-0.1f, -0.02f, -2.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);//kanan 2
+        gl.glColor3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, -2.0f);
+        gl.glVertex3f(0.0f, 0.0f, 2.0f);
+        gl.glVertex3f(0.0f, -0.02f, 2.0f);
+        gl.glVertex3f(0.0f, -0.02f, -2.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);//kiri 2
+        gl.glColor3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(-0.1f, 0.0f, -2.0f);
+        gl.glVertex3f(-0.1f, 0.0f, 2.0f);
+        gl.glVertex3f(-0.1f, -0.02f, 2.0f);
+        gl.glVertex3f(-0.1f, -0.02f, -2.0f);
+        gl.glEnd();
+    //    angle += 0.5;
+    }
+        static void balingbesar(GL gl){
+             gl.glBegin(GL.GL_POLYGON);//awal atas
+        gl.glVertex3f(5f, 0f, 0f);
+       gl.glVertex3f(5f, 1f, 0f);
+        gl.glVertex3f(-5f, 0f, 0f);
+       gl.glVertex3f(-5f, 1f, 0f);
+        gl.glEnd();
+         gl.glBegin(GL.GL_POLYGON);//bawah
+        gl.glVertex3f(5f, 0f, -0.5f);
+        gl.glVertex3f(5f, 1f, -0.5f);
+        gl.glVertex3f(-5f, 0f, -0.5f);
+       gl.glVertex3f(-5f, 1f, -0.5f);
+        gl.glEnd();
+         gl.glBegin(GL.GL_POLYGON);//tutup 1
+        gl.glVertex3f(5f, 1f, -0.5f);
+        gl.glVertex3f(5f, 1f, 0f);
+        gl.glVertex3f(-5f, 1f, -0.5f);
+        gl.glVertex3f(-5f, 1f, 0f);
+        gl.glEnd();
+         gl.glBegin(GL.GL_POLYGON);//bawah
+        gl.glVertex3f(5f, 1f, 0f);
+        gl.glVertex3f(5f, 1f, -0.5f);
+        gl.glVertex3f(-5f, 1f, 0f);
+        gl.glVertex3f(-5f, 1f, -0.5f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(5f, 0f, 0f);
+       gl.glVertex3f(5f, 1f, 0f);
+        gl.glVertex3f(5f, 0f, 0f);
+       gl.glVertex3f(5f, 1f, 0f);
+        gl.glEnd();
+        
+        }
+         static void ekor3(GL gl) {
+             gl.glBegin(GL.GL_POLYGON);/*  bagian depan */
+        gl.glNormal3f(-8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 0.0f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian bawah */
+        gl.glNormal3f(0.0f, 0.0f, -1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian belakang */
+        gl.glNormal3f(8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 1.5f, 0.0f);
+        gl.glVertex3f(8.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian atas */
+        gl.glNormal3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 1.5f, 1.0f);
+        gl.glVertex3f(8.0f, 0.0f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glVertex3f(0.0f, 1.0f, 1.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kiri */
+        gl.glNormal3f(0.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 0.0f);
+        gl.glVertex3f(0.0f, 1.5f, 1.0f);
+        gl.glVertex3f(0.0f, 0.0f, 1.0f);
+        gl.glEnd();
+        gl.glBegin(GL.GL_POLYGON);/*  bagian kanan */
+        gl.glNormal3f(0.0f, -1.5f, 0.0f);
+        gl.glVertex3f(8.0f, 0.0f, 0.0f);
+        gl.glVertex3f(8.0f, 0.0f, 1.0f);
+        gl.glVertex3f(8.0f, 1.5f, 1.0f);
+        gl.glVertex3f(8.0f, 1.5f, 0.0f);
+        gl.glEnd();
+        }
+}
